8478df127cfd4584c1474c637fff1522078fddb3
Bobur816 <boburabduxalilov1999@gmail.com>
2024-03-28 12:50:12 +0500

    change bg color to black

--- src/ui/Main.jsx
+++ src/ui/Main.jsx
@@ -4,8 +4,8 @@ import styled from "styled-components";
 import Footer from "./Footer";
 
 const StyledMain = styled.main`
-	/* background-color: rgba(26, 26, 26, 1); */
-	background-color: red;
+	background-color: rgba(26, 26, 26, 1);
+	/* background-color: red; */
 	overflow-y: scroll;
 	border-radius: 12px;
 	padding: 120px;
